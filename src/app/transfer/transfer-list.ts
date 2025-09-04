6b627b5a7e0297d92a78147c04680dba1f605532
Vitor Okubo <vitor.okubo@outlook.com>
2025-09-04 11:09:32 -0300

    Atualizando variaveis para signal

--- src/app/transfer/transfer-list.ts
+++ src/app/transfer/transfer-list.ts
@@ -1,5 +1,5 @@
 import { DatePipe } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Transfer, TransferService } from './transfer.service';
 
@@ -13,59 +13,60 @@ import { Transfer, TransferService } from './transfer.service';
 export class TransferListComponent implements OnInit {
   private transferService = inject(TransferService);
 
-  transfers: Transfer[] = [];
-  page = 1;
-  pageSize = 10;
-  totalPages = 1;
-  showModal = false;
-  errorMsg: string[] = [];
-  newTransfer: Transfer = {
+  transfers = signal<Transfer[]>([]);
+  page = signal(1);
+  pageSize = signal(10);
+  totalPages = signal(1);
+  showModal = signal(false);
+  errorMsg = signal<string[]>([]);
+  newTransfer = signal<Transfer>({
     sourceAccount: '',
     destinationAccount: '',
     amount: 0,
     transferDate: ''
-  };
+  });
 
   ngOnInit() {
-    this.loadTransfers(this.page);
+    this.loadTransfers(this.page());
   }
 
   openModal() {
-    this.showModal = true;
-    this.errorMsg.slice(0, this.errorMsg.length);
-    this.newTransfer = {
+    this.showModal.set(true);
+    this.errorMsg.set([]);
+    this.newTransfer.set({
       sourceAccount: '',
       destinationAccount: '',
       amount: 0,
       transferDate: ''
-    };
+    });
   }
 
   closeModal() {
-    this.showModal = false;
+    this.showModal.set(false);
   }
 
   submitTransfer() {
-    this.errorMsg.splice(0, this.errorMsg.length);
-    this.transferService.createTransfer(this.newTransfer)
+    this.errorMsg.set([]);
+    this.transferService.createTransfer(this.newTransfer())
       .subscribe({
         next: (resp) => {
           if (resp.status === 201) {
             this.closeModal();
-            this.loadTransfers(this.page);
+            this.loadTransfers(this.page());
           }
         },
         error: (err) => {
           const errObject = JSON.parse(JSON.stringify(err.error));
-          Object.values(errObject).forEach((textError) => this.errorMsg.push(String(textError)));
+          const errors = Object.values(errObject).map((textError) => String(textError));
+          this.errorMsg.set(errors);
         }
       });
   }
 
   goToPage(pageNumber: number): void {
-    if (pageNumber >= 1 && pageNumber <= this.totalPages) {
-      this.page = pageNumber;
-      this.loadTransfers(this.page);
+    if (pageNumber >= 1 && pageNumber <= this.totalPages()) {
+      this.page.set(pageNumber);
+      this.loadTransfers(this.page());
     }
   }
 
@@ -73,8 +74,8 @@ export class TransferListComponent implements OnInit {
     this.transferService.loadTransfers(page).subscribe({
       next: ((transfers => {
         if (transfers.content.length) {
-          this.transfers = transfers.content;
-          this.totalPages = transfers.totalPages;
+          this.transfers.set(transfers.content);
+          this.totalPages.set(transfers.totalPages);
         }
       }))
     });
